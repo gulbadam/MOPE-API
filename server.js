221a07c73b10dac0bba8c2ee485ab4f366918247
yelena guliyeva <gulbadam@gulbadam.com>
2018-06-22 12:54:10 -0700

    signin

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const PORT= 3000;
 app.get('/', (req, res)=> {
     res.send('WORKING')
 });
+app.post('/signin',(req, res)=>{
+    res.send('SIGNIN')
+});
 app.listen(PORT, () => {
     console.log(`app is running on port ${PORT}`)
 })
