57517bf465666c9f5bb1080a9a7dec463919fff0
yelena guliyeva <gulbadam@gulbadam.com>
2018-06-25 11:05:44 -0700

    image

--- server.js
+++ server.js
@@ -30,42 +30,52 @@ app.post('/signin', (req, res) => {
     
     if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
         res.json(database.users[0]);
-        console.log(data.users[0])
+        
     } else {
         res.status(400).json("error login");
         console.log("not")
     }
 })
 
-app.post('/findface', (req, res) => {
+app.put('/image', (req, res) => {
+    const {id} = req.body;
+    let found = false;
     database.users.forEach(user => {
-        if (user.email === req.body.email) {
+        if (user.id === id) {
+            found =true;
             user.entries++
-            res.json(user)
+            console.log(user.entries);
+            return res.json(user.entries)
         }
-    });
-    res.json('nope')
+    })
+    if(!found) {
+    res.status(400).json('not found')}
 })
 
 
 app.post('/register', (req, res) => {
+    const {email, name, password} =req.body;
     database.users.push({
         id: '124',
-        name: req.body.name,
-        email: req.body.email,
+        name: name,
+        email: email,
         entries: 0,
         joined: new Date()
     })
     res.json(database.users[database.users.length - 1])
 })
 
-app.get('/profile/:userId', (req, res) => {
+app.get('/profile/:id', (req, res) => {
+    const {id} =req.params;
+    let found =false;
     database.users.forEach(user => {
-        if (user.id === req.params.userId) {
+        if (user.id === id) {
             return res.json(user);
         }
     })
-    // res.json('no user')
+    if (!found) {
+        res.status(400).json('not found')
+    }
 
 })
 
