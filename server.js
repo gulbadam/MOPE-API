a81e0a1a49fcf7f9fd2beca13535e2cd8c8417b0
yelena guliyeva <gulbadam@gulbadam.com>
2018-06-25 16:50:30 -0700

    isValid

--- server.js
+++ server.js
@@ -40,14 +40,20 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => res.send(database.users));
 
 app.post('/signin', (req, res) => {
-    
-    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-        res.json(database.users[0]);
-        
-    } else {
-        res.status(400).json("error login");
-        console.log("not")
-    }
+    db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data  => { const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+        console.log(isValid)
+        if(isValid) {
+           return db.select('*').from('users')
+            .where('email', '=', req.body.email)
+            .then(user => {
+                res.json(user[0])
+            })
+            .catch(err =>res.status(400).json('unable to get user'))
+        }
+    })
+  .catch(err => res.status(400).json('wrong credentials'))
 })
 
 app.put('/image', (req, res) => {
@@ -79,16 +85,30 @@ app.put('/image', (req, res) => {
 
 app.post('/register', (req, res) => {
     const {email, name, password} =req.body;
-   db('users')
-   .returning("*")
-   .insert({
-       email: email,
-       name: name,
-       joined: new Date()
-   }).then(user=> {
-res.json(user[0]);
-   })
-   .catch(res => res.status(400).json("unable to register"))
+    const hash =bcrypt.hashSync(password);
+    db.transaction(trx =>{
+      trx.insert({
+          hash: hash,
+          email: email
+      }) 
+      .into("login")
+    .returning('email')
+    .then(loginEmail =>{
+return trx('users')
+    .returning("*")
+    .insert({
+        email: loginEmail[0],
+        name: name,
+        joined: new Date()
+    })
+    .then(user => {
+        res.json(user[0]);
+    })
+    })
+    .then(trx.commit)
+    .catch(trx.rollback)
+    })
+   .catch(err => res.status(400).json("unable to register"))
     
 })
 
