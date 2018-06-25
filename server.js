935b85edf346c3f49f5df201b06e3e219d22c38d
yelena guliyeva <gulbadam@gulbadam.com>
2018-06-25 14:25:25 -0700

    register err

--- server.js
+++ server.js
@@ -68,12 +68,17 @@ app.put('/image', (req, res) => {
 
 app.post('/register', (req, res) => {
     const {email, name, password} =req.body;
-   db('users').insert({
+   db('users')
+   .returning("*")
+   .insert({
        email: email,
        name: name,
        joined: new Date()
-   }).then(console.log)
-    res.json(database.users[database.users.length - 1])
+   }).then(user=> {
+res.json(user[0]);
+   })
+   .catch(err => res.status(400).json("unable to register"))
+    
 })
 
 app.get('/profile/:id', (req, res) => {
