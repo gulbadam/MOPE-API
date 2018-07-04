3b7299ff39c6faaade7bdeb896b55fb04e907417
yelena guliyeva <gulbadam@gulbadam.com>
2018-07-04 12:19:22 -0700

    auth

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const register =require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+const auth = require('./controllers/authorization');
 app.use(bodyParser.urlencoded({
     extended: false
 }));
@@ -32,12 +33,14 @@ app.use(cors());
 //app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 app.post('/signin', signin.signinAuthentication(db, bcrypt))
 
-app.put('/image', (req, res) => {console.log("body" + req.body); image.handleImage(req, res, db)})
+app.put('/image', auth.requireAuth, (req, res) => {console.log("body" + req.body); image.handleImage(req, res, db)})
 
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
-app.post('/colors', (req, res) => {image.handleApiColors(req, res)})
-app.post('/demographics', (req, res) => {image.handleApiDemographics(req, res)})
-app.post('/general', (req, res) => {image.handleApiGeneral(req, res)})
+app.post('/colors',  (req, res) => {image.handleApiColors(req, res)})
+app.post('/demographics',  (req, res) => {image.handleApiDemographics(req, res)})
+app.post('/general',  (req, res) => {image.handleApiGeneral(req, res)})
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+app.get('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileGet(req, res, db)})
+app.post('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileUpdate(req, res, db)})
 app.listen(PORT, () => {console.log(`app is running on port ${PORT}`);
 });
