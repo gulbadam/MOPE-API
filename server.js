18bcb6ec245d124bbfbe9d37bf9c6cf1408506a4
yelena guliyeva <gulbadam@gulbadam.com>
2018-07-05 09:52:48 -0700

    fixing

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const register =require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
-const auth = require('./controllers/authorization');
+
 
 const PORT = process.env.PORT || 3001 ;
 
@@ -46,9 +46,9 @@ app.post('/general',  (req, res) => {image.handleApiGeneral(req, res)})
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 app.get('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileGet(req, res, db)})
 app.post('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileUpdate(req, res, db)})
-//app.listen(PORT, () => {console.log(`app is running on port ${PORT}`);
+app.listen(PORT, () => {console.log(`app is running on port ${PORT}`);
 
-//});
+});
 app.listen(process.env.PORT || 3001, function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
