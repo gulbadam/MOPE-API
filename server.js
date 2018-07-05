fc17ef95a210c105c63646b44b5137849d7ace85
yelena guliyeva <gulbadam@gulbadam.com>
2018-07-05 09:55:37 -0700

    server

--- server.js
+++ server.js
@@ -35,17 +35,17 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => res.send("working"));
 
 //app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
-app.post('/signin', signin.signinAuthentication(db, bcrypt))
+app.post('/signin', signin.handleSignin(db, bcrypt))
 
-app.put('/image', auth.requireAuth, (req, res) => {console.log("body" + req.body); image.handleImage(req, res, db)})
+app.put('/image', (req, res) => {console.log("body" + req.body); image.handleImage(req, res, db)})
 
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 app.post('/colors',  (req, res) => {image.handleApiColors(req, res)})
 app.post('/demographics',  (req, res) => {image.handleApiDemographics(req, res)})
 app.post('/general',  (req, res) => {image.handleApiGeneral(req, res)})
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
-app.get('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileGet(req, res, db)})
-app.post('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileUpdate(req, res, db)})
+app.get('/profile/:id',  (req, res) => { profile.handleProfileGet(req, res, db)})
+//app.post('/profile/:id',  (req, res) => { profile.handleProfileUpdate(req, res, db)})
 app.listen(PORT, () => {console.log(`app is running on port ${PORT}`);
 
 });
