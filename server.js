efbe36c7aaa5782548376d6948c8f70f2731d572
yelena guliyeva <gulbadam@gulbadam.com>
2018-08-18 17:02:03 -0700

    redis

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const register =require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+const auth = require('./controllers/authorization');
 
 
 //const PORT = process.env.PORT || 3001 ;
@@ -17,27 +18,44 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     //connection: {
-        // host: process.env.POSTGRES_HOST,
-        // user: process.env.POSTGRES_USER,
-        // password: process.env.POSTGRES_PASSWORD,
-        // database: process.env.POSTGRES_DB
-        connection: process.env.DATABASE_URI,
+        //host: process.env.POSTGRES_HOST,
+        //user: process.env.POSTGRES_USER,
+        //password: process.env.POSTGRES_PASSWORD,
+        //database: process.env.POSTGRES_DB,
+        connection: process.env.POSTGRES_URI
         // connectionString: process.env.DATABASE_URL,
          //ssl: true
-    //}
+   // }
 });
 const app = express();
-app.use(morgan('combined'));
-app.use(cors());
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+app.use(morgan('combined', {
+            skip: function (req, res) {
+                return res.statusCode < 400
+            }
+        }));
+app.use (cors (corsOptions));
+
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(bodyParser.json());
 
-//app.get('/', (req, res) => res.send(database.users));
+app.get('/', (req, res) => res.send(database.users));
 app.get('/', (req, res) => res.send("working"));
 
-app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
+//app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
+app.post('/signin', signin.signinAuthentication(db, bcrypt));
 //app.post('/signin', (req, res) => {signin.handleSignin(db, bcrypt)})
 
 app.put('/image', (req, res) => {console.log("body" + req.body); image.handleImage(req, res, db)})
